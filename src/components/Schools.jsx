aa43afb41a7fabd842b119d634fe0d7ef85a13c0
omarbramirez <omar.ramirez94@hotmail.es>
2024-03-24 08:09:00 -0600

    schools module fixed

--- src/components/Schools.jsx
+++ src/components/Schools.jsx
@@ -1,6 +1,15 @@
 import SchoolsList from '../assets/data/schools.json';
+import Colegio01 from '../assets/images/schools/Colegio01.jpg';
+import Colegio02 from '../assets/images/schools/Colegio02.jpg';
+import Colegio03 from '../assets/images/schools/Colegio03.jpg';
+import Colegio04 from '../assets/images/schools/Colegio04.jpg';
+import Colegio05 from '../assets/images/schools/Colegio05.jpg';
+import Colegio06 from '../assets/images/schools/Colegio06.jpg';
+import Colegio07 from '../assets/images/schools/Colegio07.jpg';
+import Colegio08 from '../assets/images/schools/Colegio08.jpg';
 
 function Schools(){
+    const schoolsimgs =[[Colegio01,Colegio02],[Colegio03,Colegio04],[Colegio05,Colegio06],[Colegio07,Colegio08]]
     return(
         <>
         <div>
@@ -10,8 +19,8 @@ function Schools(){
                         <h3>{school.name} 
                         </h3>
                 </li>
-                {school.images.map((image, index)=>(
-                    <li key={`gallery-${index}`}>
+                {schoolsimgs[index].map((image, i)=>(
+                    <li key={`gallery-${school.name}`}>
                     <img src={image} alt={school.name} />
                 </li>
                     ))}
