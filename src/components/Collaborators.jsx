90a695daceb907bfb20afcd0d12d01bbf24bd391
omarbramirez <omar.ramirez94@hotmail.es>
2024-03-24 07:39:12 -0600

    testing images

--- src/components/Collaborators.jsx
+++ src/components/Collaborators.jsx
@@ -1,9 +1,26 @@
 import CollaboratorsList from '../assets/data/collaborators.json';
+import Abril from '../assets/images/collaborators/abril.png'; 
 
 function Collaborators(){
     return(
         <>
         <div>
+
+        <ul>
+
+<li>
+    <img src={Abril} alt="Abril Altamirano" />
+</li>
+<li>
+        <p>Abril Altamirano
+        <br/>
+        Editora y escritora.
+        </p>
+
+</li>
+</ul>
+
+
 {CollaboratorsList.map((collaborator, index)=>(
             <ul key={`collaborator-${index}`}>
 
